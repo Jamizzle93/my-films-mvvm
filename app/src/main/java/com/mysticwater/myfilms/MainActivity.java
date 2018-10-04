eccad3076dc208b093fdfe2dd178d805d967f600
James Warner <james.warner@raspberrysoftware.com>
2018-10-04 20:27:52 +0100

    feat(MainActivity): Implement method to setup the toolbar

--- app/src/main/java/com/mysticwater/myfilms/MainActivity.java
+++ app/src/main/java/com/mysticwater/myfilms/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mysticwater.myfilms;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(R.string.app_name);
+        }
     }
 
 }
