79cae99ff323633f1e02d1dd01d089ea3457a376
James Warner <james.warner@raspberrysoftware.com>
2018-10-09 21:12:05 +0100

    feat(FilmsList): Setup the films recycler view and add a test film

--- app/src/main/java/com/mysticwater/myfilms/FilmsFragment.java
+++ app/src/main/java/com/mysticwater/myfilms/FilmsFragment.java
@@ -3,9 +3,14 @@ package com.mysticwater.myfilms;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilmsFragment extends Fragment {
 
@@ -20,7 +25,26 @@ public class FilmsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_films, null);
+
+        View rootView = inflater.inflate(R.layout.fragment_films, null);
+
+        setupFilmsRecyclerView(rootView);
+
+        return rootView;
     }
+
+    private void setupFilmsRecyclerView(View rootView) {
+        RecyclerView filmsRecyclerView = rootView.findViewById(R.id.recycler_view_films);
+        filmsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        List<Film> films = new ArrayList();
+        Film testFilm = new Film(1, "Film Title", "01/01/1970", "", 2.5, 2);
+        films.add(testFilm);
+        FilmsAdapter adapter = new FilmsAdapter(films);
+
+        filmsRecyclerView.setAdapter(adapter);
+    }
+
+
 
 }
