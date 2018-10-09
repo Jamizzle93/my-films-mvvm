e215ffadb8d79a2d32235c467d3d5c8e9eb1209f
James Warner <james.warner@raspberrysoftware.com>
2018-10-09 19:13:54 +0100

    feat(FilmsFragment): Implement an initial fragment for the films

---
+++ app/src/main/java/com/mysticwater/myfilms/FilmsFragment.java
@@ -0,0 +1,26 @@
+package com.mysticwater.myfilms;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FilmsFragment extends Fragment {
+
+    public FilmsFragment() {
+    }
+
+    public static FilmsFragment newInstance() {
+        return new FilmsFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_films, null);
+    }
+
+}
