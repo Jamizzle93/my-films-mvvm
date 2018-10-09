ec4af77294fc06cf48dec5282c60d3fe3003127a
James Warner <james.warner@raspberrysoftware.com>
2018-10-09 20:50:13 +0100

    feat(FilmsAdapter): Update onBindViewHolder to set film data

--- app/src/main/java/com/mysticwater/myfilms/FilmsAdapter.java
+++ app/src/main/java/com/mysticwater/myfilms/FilmsAdapter.java
@@ -3,6 +3,7 @@ package com.mysticwater.myfilms;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatRatingBar;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,10 @@ import java.util.List;
 public class FilmsAdapter extends RecyclerView.Adapter<FilmsAdapter.ViewHolder> {
 
     private List<Film> films;
+
+    public FilmsAdapter(List<Film> films) {
+        this.films = films;
+    }
 
     @NonNull
     @Override
@@ -27,7 +32,33 @@ public class FilmsAdapter extends RecyclerView.Adapter<FilmsAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         Film film = films.get(i);
 
-        // TODO - Populate film
+        String filmTitle = film.getTitle();
+        if (TextUtils.isEmpty(filmTitle)) {
+            filmTitle = "";
+        }
+        viewHolder.title.setText(filmTitle);
+
+        String filmReleaseDate = film.getReleaseDate();
+        if (TextUtils.isEmpty(filmReleaseDate)) {
+            filmReleaseDate = "";
+        }
+        viewHolder.releaseDate.setText(filmReleaseDate);
+
+        String filmVoteAverageText = "";
+        double filmVoteAverage = film.getVoteAverage();
+        if (filmVoteAverage > 0) {
+            filmVoteAverageText = String.valueOf(filmVoteAverage);
+        }
+        viewHolder.voteAverage.setText(filmVoteAverageText);
+
+        String filmVoteCountText = "";
+        int filmVoteCount = film.getVoteCount();
+        if (filmVoteCount > 0) {
+            filmVoteCountText = String.valueOf(filmVoteAverage);
+        }
+        viewHolder.voteCount.setText(filmVoteCountText);
+
+        // TODO - Load poster path
     }
 
     @Override
