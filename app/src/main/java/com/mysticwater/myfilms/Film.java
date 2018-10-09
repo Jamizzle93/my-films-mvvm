e1278aeb72d8fea900627f49d9f8c9bdb887187a
James Warner <james.warner@raspberrysoftware.com>
2018-10-09 20:12:09 +0100

    feat(*): Implement the Film model class

---
+++ app/src/main/java/com/mysticwater/myfilms/Film.java
@@ -0,0 +1,22 @@
+package com.mysticwater.myfilms;
+
+public class Film {
+
+    private long id;
+    private String title;
+    private String releaseDate;
+    private String posterPath;
+    private double voteAverage;
+    private int voteCount;
+
+    public Film(long id, String title, String releaseDate, String posterPath, double voteAverage,
+                int voteCount) {
+        this.id = id;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.posterPath = posterPath;
+        this.voteAverage = voteAverage;
+        this.voteCount = voteCount;
+    }
+
+}
