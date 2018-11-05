b2731c8953c88c987b8cc6618ef50f05eb081c88
James Warner <james.warner@raspberrysoftware.com>
2018-11-05 20:09:31 +0000

    fix(setImageBinding): Improve check for image url

--- app/src/main/java/com/mysticwater/myfilms/FilmBindingAdapters.java
+++ app/src/main/java/com/mysticwater/myfilms/FilmBindingAdapters.java
@@ -1,6 +1,7 @@
 package com.mysticwater.myfilms;
 
 import android.databinding.BindingAdapter;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -9,7 +10,7 @@ public class FilmBindingAdapters {
 
     @BindingAdapter("imageUrl")
     public static void setImageUrl(ImageView imageView, String imageUrl) {
-        if (imageUrl == null) {
+        if (TextUtils.isEmpty(imageUrl)) {
             imageView.setImageDrawable(null);
         } else {
             Picasso.get().load(imageUrl).into(imageView);
