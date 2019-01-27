93920600034a4824f84c36cd7a45f7ec421fb369
James Warner <james.warner@raspberrysoftware.com>
2019-01-27 19:06:35 +0000

    feat(network): Add 'getUpcomingFilms' network call

--- app/src/main/java/com/mysticwater/myfilms/network/TheMovieDbService.java
+++ app/src/main/java/com/mysticwater/myfilms/network/TheMovieDbService.java
@@ -1,16 +1,26 @@
 package com.mysticwater.myfilms.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mysticwater.myfilms.data.model.FilmResults;
+import com.mysticwater.myfilms.util.GsonCalendarDeserializer;
+
+import java.util.Calendar;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface TheMovieDbService {
-    @GET("movie/popular")
-    Call<FilmResults> getUpcomingFilms();
+    @GET("discover/movie?3|2")
+    Call<FilmResults> getUpcomingFilms(
+            @Query("with_release_type") String releaseType,
+            @Query("primary_release_date.gte") String startDate,
+            @Query("primary_release_date.lte") String endDate
+    );
 
     class Creator {
         public static TheMovieDbService newMovieService() {
