dcc62d7b1368e6d365e78e078ddf54d69de64dd1
James Warner <james.warner@raspberrysoftware.com>
2019-05-24 14:43:07 +0100

    feat(*): Include Stetho library

--- app/src/main/java/com/mysticwater/myfilms/FilmsApplication.java
+++ app/src/main/java/com/mysticwater/myfilms/FilmsApplication.java
@@ -2,6 +2,7 @@ package com.mysticwater.myfilms;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
 import com.mysticwater.myfilms.data.FilmsRepository;
 
 public class FilmsApplication extends Application {
@@ -9,6 +10,8 @@ public class FilmsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Stetho.initializeWithDefaults(this);
     }
 
     public FilmsRepository getFilmsRepository() {
